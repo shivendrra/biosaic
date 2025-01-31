944dbab0647b7ab266381561fbdb9b3e3f452213
Shivendra <shivharsh44@gmail.com>
2025-02-01 00:48:03 +0530

    ateast the python version works as anticipated

--- test.py
+++ test.py
@@ -1,29 +1,19 @@
-import os
+from src.kmer import KMerPy
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
-os.chdir(current_directory)
+token = KMerPy(kmer_size=4)
 
-from src import KMer
+# with open("file.txt", "r", encoding="utf-8") as f:
+#   sequence = f.read()
+#   print("sequence length: ", len(sequence))
+#   f.close()
 
-CHUNK_SIZE = 1000000
+sequence = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGCTTCTGAACTG"
+token.build_vocab()
+token.save("./model")
+token.load("./model/base_4k.json")
+encoded = token.encode(sequence)
+decoded = token.decode(encoded)
 
-with open("file.txt", "r", encoding="utf-8") as f:
-  test_data = f.read()
-
-tokenizer = KMer(kmer=4)
-encoded_chunks = []
-decoded_chunks = []
-
-for i in range(0, len(test_data), CHUNK_SIZE):
-  chunk = test_data[i:i + CHUNK_SIZE]
-  print(f"Processing chunk {i // CHUNK_SIZE + 1}/{-(-len(test_data) // CHUNK_SIZE)}...")
-  shredded_chunk = tokenizer._shred(chunk)
-  encoded_chunk = tokenizer.encode(chunk)
-  decoded_chunk = tokenizer.decode(encoded_chunk)
-  encoded_chunks.extend(encoded_chunk)
-  decoded_chunks.append(decoded_chunk)
-
-decoded_result = ''.join(decoded_chunks)
-print(decoded_result)
-print("Decoded matches original:", decoded_result == test_data)
-del tokenizer
+print(encoded)
+print(decoded)
+print(decoded == sequence)
