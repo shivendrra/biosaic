5c4b661ef0dfa57b6aa098729f1d5c7ddb8093ac
Shivendra <shivharsh44@gmail.com>
2025-02-01 01:39:20 +0530

    create a common base class for further loigcs

--- src/csrc/base.c
+++ src/csrc/base.c
@@ -5,51 +5,77 @@
 #include "inc/tqdm.h"
 #include "base.h"
 
-void init_tokenizer(BaseTokenizer* tokenizer) {}
-
-void build_vocab(BaseTokenizer* tokenizer) {
+void init_tokenizer(BaseTokenizer* tokenizer, int kmer_size) {
   if (!tokenizer) {
-    fprintf(stderr, "No instance of KMer class found!");
+    fprintf(stderr, "Tokenizer instance is NULL!\n");
     exit(EXIT_FAILURE);
   }
-  const char* chars = tokenizer->base_chars;
-  int num_chars = strlen(tokenizer->base_chars), max_k = tokenizer->kmer_size;
-  int index = 0;
+  tokenizer->kmer_size = kmer_size;
+  tokenizer->vocab_size = 0;
+  // vocab_size is basically ``summation from i=0 to n=chars_size len(self->base_chars)^kmer_size``, since we're trying to create each
+  // possible token -> idx pair till the declared KMer size
+  // so if kmer = 4:
+  //        vocab_size = 5 + 25 + 125 + 625 = 780
+  int vocab_size = 0;
+  for (int i = 0; i < kmer_size; i++) {
+    vocab_size += pow(strlen(BASE_CHARS), i);
+  }
+  tokenizer->vocab_size += strlen(SPECIAL_CHARS);
+  tokenizer->entries = malloc(vocab_size * sizeof(KmerEntry));
+}
 
-  // adding special tokens mapping first
-  for (int i = 0; i < strlen(tokenizer->special_tokens); i++) {
-    char special[2] = { tokenizer-> special_tokens[i], '\0'};
-    tokenizer->ids_to_token[index] = strdup(special);
-    tokenizer->token_to_ids[index] = index;
-    index++;
+void build_vocab(BaseTokenizer* tokenizer) {
+  if (!tokenizer || !tokenizer->entries) {
+    fprintf(stderr, "Tokenizer is not initialized properly!\n");
+    exit(EXIT_FAILURE);
   }
 
-  tqdm bar; // initialized tqdm bar
-  init_tqdm(&bar, "Building the vocab: ", false, "KMers", true, tokenizer->vocab_size - strlen(tokenizer->special_tokens) , 1);
+  int index = 0;
 
-  // adding the base_char pairs mapping
-  for (int k = 0; k <= max_k; k++) {
-    int* indices = (int*)malloc(k * sizeof(int));
-    char* combination = malloc((k + 1) * sizeof(char));
-    if (!indices || !combination) {
-      fprintf(stderr, "Memory allocation failed!\n");
+  for (int i = 0; i < strlen(SPECIAL_CHARS); i++) {
+    tokenizer->entries[index].idx = index;
+    tokenizer->entries[index].val = (char*)malloc(2); // allocating space for token + '\0'
+    if (!tokenizer->entries[index].val) {
+      fprintf(stderr, "Memory allocation for special token failed at index %d\n", index);
       exit(EXIT_FAILURE);
     }
-    combination[k] = '\0';
+    tokenizer->entries[index].val[0] = SPECIAL_CHARS[i];
+    tokenizer->entries[index].val[1] = '\0';
+    index++;
+  }
+  tqdm bar; // initialized tqdm bar
+  init_tqdm(&bar, "Building the vocab: ", false, "KMers", true, tokenizer->vocab_size - strlen(SPECIAL_CHARS) , 1);
+
+  for (int k = 1; k <= tokenizer->kmer_size; k++) {
+    int* indices = (int*)malloc(k * sizeof(int));
+    char* combination = (char*)malloc((k + 1) * sizeof(char));
+    if (!indices || !combination) {
+      fprintf(stderr, "Memory allocation failed for k-mer generation!\n");
+      exit(EXIT_FAILURE);
+    }
 
     for (int i = 0; i < k; i++) indices[i] = 0;
+    combination[k] = '\0';
+
     while (1) {
       for (int i = 0; i < k; i++) {
-        combination[i] = chars[indices[i]];
+        combination[i] = BASE_CHARS[indices[i]];
       }
-      tokenizer->ids_to_token[index] = strdup(combination);
-      tokenizer->token_to_ids[index] = index;
+
+      tokenizer->entries[index].idx = index;
+      tokenizer->entries[index].val = (char*)malloc((k + 1) * sizeof(char));
+      if (!tokenizer->entries[index].val) {
+        fprintf(stderr, "Memory allocation failed for token at index %d\n", index);
+        exit(EXIT_FAILURE);
+      }
+      memcpy(tokenizer->entries[index].val, combination, k + 1);
       index++;
       update_tqdm(&bar, 1, index == tokenizer->vocab_size);
+      fflush(stdout);
 
       int i;
-      for (i = k - 1; i >= 0; i++) {
-        if (indices[i] < num_chars - 1) {
+      for (i = k - 1; i >= 0; i--) {
+        if (indices[i] < strlen(BASE_CHARS) - 1) {
           indices[i]++;
           break;
         }
@@ -57,51 +83,20 @@ void build_vocab(BaseTokenizer* tokenizer) {
       }
       if (i < 0) break;
     }
+
     free(indices);
     free(combination);
   }
   close_tqdm(&bar);
 }
 
-void save_tokenizer(BaseTokenizer* tokenizer, const char* path) {
-  if (!tokenizer) {
-    fprintf(stderr, "No instance of KMer class found!");
-    exit(EXIT_FAILURE);
-  }
-  char model_file[100];
-  snprintf(model_file, 100, "%s.model", path);
-  FILE* file = fopen(model_file, "w");
-  if (!file) {
-    printf("Error opening file for saving model.\n");
-    return;
-  }
-  char temp[MAX_TOKEN_SIZE];
-  for (int i = 0; i < tokenizer->vocab_size; i++) {
-    strncpy(temp, tokenizer->ids_to_token[i], MAX_TOKEN_SIZE - 1);
-    temp[MAX_TOKEN_SIZE - 1] = '\0';
-
-    for (int j = 0; temp[j] != '\0'; j++) {
-      if (temp[j] == '\n') {
-        temp[j] = 'n';
-      }
-    }
-    fprintf(file, "\"%s\" %d\n", temp, i + 1);
-  }
-  fclose(file);
-  printf("Model saved to %s\n", path);
-}
-
-void load_tokenizer(BaseTokenizer* tokenizer, const char* model_file) {}
-
 void free_tokenizer(BaseTokenizer* tokenizer) {
   if (!tokenizer) {
-    fprintf(stderr, "No instance of KMer class found!");
+    fprintf(stderr, "No instance of BaseTokenizer class found!");
     exit(EXIT_FAILURE);
   }
-  for (int i = 0; i < tokenizer->vocab_size; i++) {
-    free(tokenizer->ids_to_token[i]);
-  }
-  free(tokenizer->ids_to_token);
-  free(tokenizer->token_to_ids);
+  free(tokenizer->entries->idx);
+  free(tokenizer->entries->val);
+  free(tokenizer->entries);
   free(tokenizer);
 }
--- src/csrc/base.h
+++ src/csrc/base.h
@@ -1,3 +1,10 @@
+/*
+  @base.h
+  * base class for KMer tokenization processes
+    ** has necessary building blocks for further classes
+  * to be compiled with the respective file containing the main logic
+*/
+
 #ifndef __BASE__H__
 #define __BASE__H__
 
@@ -6,18 +13,23 @@
 #define  MAX_MERGES  10000
 #define  MAX_TOKEN_SIZE  6
 
+#define  BASE_CHARS  "ATCG\n"
+#define  SPECIAL_CHARS  "MPSE "
+
 typedef struct {
-  char base_chars[BASE_VOCAB_SIZE];
-  char special_tokens[MAX_SPECIAL_TOKENS];
+  int idx;
+  char* val;
+} KmerEntry;
+
+typedef struct {
   int kmer_size;
   int vocab_size;
-  char** ids_to_token;
-  int* token_to_ids;
+  KmerEntry* entries;
 } BaseTokenizer;
 
-void init_tokenizer(BaseTokenizer* tokenizer);
+void init_tokenizer(BaseTokenizer* tokenizer, int kmer_size);
 void build_vocab(BaseTokenizer* tokenizer);
-void save_tokenizer(const BaseTokenizer* tokenizer, const char* file_path);
+void save_tokenizer(BaseTokenizer* tokenizer, const char* file_path);
 void load_tokenizer(BaseTokenizer* tokenizer, const char* model_file);
 void free_tokenizer(BaseTokenizer* tokenizer);
 
