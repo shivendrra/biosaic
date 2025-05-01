2f33ce0d144c7f89c98c062a9114ec1ebb583c77
Shivendra <shivharsh44@gmail.com>
2025-05-01 14:16:32 +0530

    added automatic genomic data downloading script

--- biosaic/__init__.py
+++ biosaic/__init__.py
@@ -2,4 +2,5 @@ from .main import tokenizer
 from .main import pre_encoding as get_encodings
 from .main import pre_model as get_models
 from .main import pre_mode as get_modes
-from .process import consolidate, parquet_to_csv, parquet_to_text, split_file, unzip
+from .database import get_database
+from .process import consolidate, parquet_to_csv, parquet_to_text, split_file, unzip, cleanse_db
